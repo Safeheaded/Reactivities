bdf284b94d263ba605f23a84d2584241aa2c5956
safeheaded <m.patryk@outlook.com>
2020-06-06 15:29:30 +0200

    End of Section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Header, Icon, List } from 'semantic-ui-react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [values, setValues] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(
+				'http://localhost:5000/api/values'
+			);
+			setValues(response.data);
+		})();
+	});
+
+	return (
+		<div>
+			<Header as='h2'>
+				<Icon name='users' />
+				<Header.Content>Reactivities</Header.Content>
+			</Header>
+			<List>
+				{values.map((value: any) => (
+					<List.Item key={value.id}>
+						{value.name}
+					</List.Item>
+				))}
+			</List>
+		</div>
+	);
 }
 
 export default App;
